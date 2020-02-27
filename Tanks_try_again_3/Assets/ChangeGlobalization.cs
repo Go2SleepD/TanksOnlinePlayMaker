34c51f50c0f24be956812e49b1d0f1a8e5adb675
Go2Sleep <55402914+Go2SleepD@users.noreply.github.com>
2020-02-27 17:09:39 +0300

    Add project files

---
+++ Tanks_try_again_3/Assets/ChangeGlobalization.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Globalization;
+using System.Threading;
+
+public class ChangeGlobalization : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(840, 600, false);
+        var culture = new CultureInfo("en-US");
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tanks_try_again_3/Assets/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject tilemapGameObject;
+
+    Tilemap tilemap;
+
+    void Start()
+    {
+        tilemapGameObject = GameObject.FindGameObjectWithTag("Bricks");
+        if (tilemapGameObject != null)
+        {
+            tilemap = tilemapGameObject.GetComponent<Tilemap>();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tilemap != null && tilemapGameObject == collision.gameObject)
+        {
+            Vector3 hitPosition = Vector3.zero;
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                Debug.Log("YOU SHOT BLOCK");
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            }
+        }
+    }
+}
